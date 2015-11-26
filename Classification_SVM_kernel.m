095cec159afacf623a7b7809203c982aa172a785
Mika <mikael.jorda@gmail.com>
2015-11-26 15:48:03 -0800

    move data files in folder and added Classification_SVM_kernel

---
+++ Classification_SVM_kernel.m
@@ -0,0 +1,25 @@
+% perform multi class SVM on data with a kernel
+
+% clear all
+% close all
+
+addpath('../Matlab/libsvm-3.20/matlab');  % add LIBSVM to Mika path
+
+%addpath('../../../Software/liblinear-2.1/matlab'); % add LIBSVM to Nina path
+
+%% chose feature type
+filename = 'data_features/BoWpca_data.txt';
+str = sprintf('Loading images representations from file %s...', filename);
+fprintf(str);
+M = dlmread(filename,'');
+sparseMatrix = sparse(M);
+fprintf('done.\n');
+
+label_vector = dlmread('data_features/labels.txt');
+
+%% options
+
+options = '-v 10 -q';
+
+%% train model
+SVM_model = svmtrain(label_vector,sparseMatrix,options);
